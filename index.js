9d0807b72b03b515db0389c884e4cd2826f71de0
Sancesco <scesco7@gmail.com>
2025-06-02 14:19:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Toki está en marcha 🚀</h1>
+      <p>Escanea un QR para acceder a la mesa.</p>
+    </div>
+  );
+}
